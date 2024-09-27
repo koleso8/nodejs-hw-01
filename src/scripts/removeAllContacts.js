264548c40475d4c76e81b002688bcad295391032
koleso8 <dev.koleso@gmail.com>
2024-09-27 17:08:15 +0300

    fix hw

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,6 @@
 import { writeContacts } from '../utils/writeContacts.js';
 
-export const removeAllContacts = async () => {};
-await writeContacts([]);
+export const removeAllContacts = async () => {
+  await writeContacts([]);
+};
 removeAllContacts();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -4,7 +4,7 @@ import fs from 'node:fs/promises';
 export const writeContacts = async updatedContacts => {
   const data = await updatedContacts;
   try {
-    await fs.writeFile(PATH_DB, JSON.stringify(data));
+    await fs.writeFile(PATH_DB, JSON.stringify(data, null, 2));
   } catch (err) {
     console.error(`Не додано :${err}`);
   }
